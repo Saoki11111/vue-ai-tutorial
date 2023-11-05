f991e66a34b1bce2032a675328a8a1fdf84e1d0f
Saoki11111 <t.aoki11111@gmail.com>
2023-11-05 14:53:36 +0900

    python REST API の実装

--- app/python/main.py
+++ app/python/main.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.get('/helloworld')
+def get_hello_message():
+  return {"message": "Hello World!"}
+
+@app.get('/api/{message}')
+def get_any_message(message: str):
+  return {"message": message}
+
+class SchemaOfTitanicFeaturesRequest(BaseModel):
+  Sex: str
+  Pclass: str
+  Age: int
+  Parch: int
+  SibSp: int
+
+@app.post('/api/titanic', response_model=SchemaOfTitanicFeaturesRequest)
+def derive_score(request_body: SchemaOfTitanicFeaturesRequest):
+  return request_body
